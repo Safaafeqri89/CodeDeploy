9e56a5cb6a5539261a466294d6ecdefb4d2bfe2a
Safaafeqri89 <113602098+Safaafeqri89@users.noreply.github.com>
2024-02-11 14:07:42 +0100

    Delete holamundo-main directory

--- holamundo-main/codedeploy-scripts/beforeinstall.sh
+++
@@ -1,5 +0,0 @@
-#bin/bash
-chmod+x /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/gradlew
- cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/
-./gradlew build
-.gradlew war
