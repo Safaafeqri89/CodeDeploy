1c78970deef0db3f7f847095a6ee460c55131260
Safaafeqri89 <113602098+Safaafeqri89@users.noreply.github.com>
2024-02-11 14:18:48 +0100

    Delete holamundo-daweb-main directory

--- holamundo-daweb-main/codedeploy-scripts/beforeinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-chmod +x /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/gradlew
-
-cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
-./gradlew build
-./gradlew war
